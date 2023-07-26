d7370943f1b1a7140ede7c185c67ee372f4abb06
Lucas Petit-LeTirilly <lucas.letirilly.petit@gmail.com>
2023-07-26 16:30:35 +0200

    Ajout du middleware d'authentification

--- routes/book.js
+++ routes/book.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 const bookCtrl = require('../controllers/book');
+const auth = require('../middleware/auth');
 
-router.post('/', bookCtrl.createBook);
+router.post('/', auth, bookCtrl.createBook);
 router.get('/', bookCtrl.getAllBooks);
 router.get('/:id', bookCtrl.getOneBook);
-router.put('/:id', bookCtrl.modifyBook);
-router.delete('/:id', bookCtrl.deleteBook);
+router.put('/:id', auth, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
 
 
 module.exports = router;
