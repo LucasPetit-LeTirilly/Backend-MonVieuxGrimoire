9c79262a5f0bfd19714e1406cbd86eb49b5b5d87
Lucas Petit-LeTirilly <lucas.letirilly.petit@gmail.com>
2023-07-26 18:49:06 +0200

    Added auto-creation of folder images if it doesn't exist when an image is uploaded

--- middleware/image-compression.js
+++ middleware/image-compression.js
@@ -1,7 +1,13 @@
 const sharp = require('sharp');
+const fs = require('fs');
 
 
 const imageCompression = async (req, res, next) => {
+  fs.access("./images", (error) => {
+    if (error) {
+      fs.mkdirSync("./images");
+    }
+  });
   const { buffer, originalname } = req.file;
   const timestamp = Date.now();
   const ref = `${timestamp}-${originalname}.webp`;
